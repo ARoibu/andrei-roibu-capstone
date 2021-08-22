3535e4d8afef9a794f77cf8da6afd5cf151aed95
ARoibu <aroibu@uwo.ca>
2021-08-22 19:36:11 -0400

    styled log display responsively

--- client/src/components/DisplayLog/DisplayLog.js
+++ client/src/components/DisplayLog/DisplayLog.js
@@ -1,21 +1,41 @@
-
+import './DisplayLog.scss'
 export default function DisplayLog({logs}) {
     return(
-        <div>
+        <div className='log'>
             {logs
             .map((data) => {
                 return(
-                    <div>
-                    <p>{data.date}</p>
-                    <p>{data.exercise}</p>
-                    <p>Set 1 Weight: {data.weight}</p>
-                    <p>Set 1 Reps: {data.reps}</p>
-                    <p>Set 2 Weight: {data.weight2}</p>
-                    <p>Set 2 Reps: {data.reps2}</p>
-                    <p>Set 3 Weight: {data.weight3}</p>
-                    <p>Set 3 Reps: {data.reps3}</p>
-                    <p>Set 4 Weight: {data.weight4}</p>
-                    <p>Set 4 Reps: {data.reps4}</p>
+                    <div className='log__container'>
+                    <h2 className='log__date'>{data.date}</h2>
+                    <h3 className='log__exercise'>{data.exercise}</h3>
+                    <table className='log__table'>
+                        <tr>
+                            <td></td>
+                            <th>Weight (lbs)</th>
+                            <th>Reps</th>
+                        </tr>
+                        <tr>
+                            <th>Set 1</th>
+                            <td>{data.weight}</td>
+                            <td>{data.reps}</td>
+                        </tr>
+                        <tr>
+                            <th>Set 2</th>
+                            <td>{data.weight2}</td>
+                            <td>{data.reps2}</td>
+                        </tr>
+                        <tr>
+                            <th>Set 3</th>
+                            <td>{data.weight3}</td>
+                            <td>{data.reps3}</td>
+                        </tr>
+                        <tr>
+                            <th>Set 4</th>
+                            <td>{data.weight4}</td>
+                            <td>{data.reps4}</td>
+                        </tr>
+                    </table>
+                    
                     </div>
                 );
             })}
